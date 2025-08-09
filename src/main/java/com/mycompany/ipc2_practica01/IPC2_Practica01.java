cb7de730a0650e1ec58cb844ba585b36a3f5690d
JFgarcia07 <jgarcianatareno@gmail.com>
2025-08-09 11:29:45 -0600

    Practica 1 ipc2

---
+++ src/main/java/com/mycompany/ipc2_practica01/IPC2_Practica01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc2_practica01;
+
+/**
+ *
+ * @author jgarcia07
+ */
+public class IPC2_Practica01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
