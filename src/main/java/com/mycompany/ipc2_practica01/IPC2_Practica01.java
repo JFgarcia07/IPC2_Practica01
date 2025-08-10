272585f914ee9bd9dd43b73df38963cd18ba4ab6
JFgarcia07 <jgarcianatareno@gmail.com>
2025-08-09 20:03:46 -0600

    Creacion la mayoria del GUI pero falta el backend

--- src/main/java/com/mycompany/ipc2_practica01/IPC2_Practica01.java
+++ src/main/java/com/mycompany/ipc2_practica01/IPC2_Practica01.java
@@ -5,6 +5,7 @@
 package com.mycompany.ipc2_practica01;
 
 import com.mycompany.ipc2_practica01.BDconnection.BDconnection;
+import com.mycompany.ipc2_practica01.GUI.TriForceEvents;
 
 /**
  *
@@ -14,14 +15,7 @@ public class IPC2_Practica01 {
 
     public static void main(String[] args) {
         System.out.println("Practica 1 IPC2");
-        BDconnection cn = new BDconnection();
-        cn.connect();
-        cn.registrarEvento("EVT-001","25/08/2025","CHARLA","Tecnología Sheikah","Auditorio Central",150);
-        cn.registrarParticipante("Zelda Hyrule","ESTUDIANTE","Universidad de Hyrule","zelda@hyrule.edu");
-        cn.inscripcion("zelda@hyrule.edu","EVT-001","ASISTENTE");
-        cn.pago("zelda@hyrule.edu","EVT-001","EFECTIVO",50.00);
-        cn.registrarActividad("ACT-001","EVT-001","CHARLA","Taller de pociones","zelda@hyrule.edu","10:00","12:00",30);
-        cn.regristrarAsistencia ("zelda@hyrule.edu","ACT-001");
-        cn.certificado("zelda@hyrule.edu","EVT-001");
+        TriForceEvents gui = new TriForceEvents();
+        gui.setVisible(true);
     }
 }
